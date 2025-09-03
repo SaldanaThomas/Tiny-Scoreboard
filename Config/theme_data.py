138e8c1a65a2675407491d2e750e5d9be60e3eea
Thomas <saldana.tom@gmail.com>
2025-09-03 10:57:51 -0700

    refactor gitignore to account for new file locations

---
+++ Config/theme_data.py
@@ -0,0 +1,76 @@
+# theme_data.py
+
+themes = ["Light", "Dark", "High Contrast", "Forest", "Ocean", "Mountain", "Sunrise", "Sunset", "Midnight", "Wine"]
+
+theme_colors = {
+    "Light": {
+        "bg": "#f0f0f0",
+        "fg": "#000000",
+        "entry_bg": "#ffffff",
+        "entry_fg": "#000000",
+        "btn_bg": "#E7E7E7"
+    },
+    "Dark": {
+        "bg": "#242424",
+        "fg": "#ffffff",
+        "entry_bg": "#525252",
+        "entry_fg": "#ffffff",
+        "btn_bg": "#3c3c3c"
+    },
+    "High Contrast": {
+        "bg": "#000000",
+        "fg": "#00FF00",
+        "entry_bg": "#001F09",
+        "entry_fg": "#00FF00",
+        "btn_bg": "#001105"
+    },
+    "Forest": {
+        "bg": "#132a13",
+        "fg": "#ecf39e",
+        "entry_bg": "#3D6D37",
+        "entry_fg": "#ecf39e",
+        "btn_bg": "#31572c"
+    },
+    "Ocean": {
+        "bg": "#0d1b2a",
+        "fg": "#e0e1dd",
+        "entry_bg": "#4A6787",
+        "entry_fg": "#e0e1dd",
+        "btn_bg": "#415a77"
+    },
+    "Mountain": {
+        "bg": "#232220",
+        "fg": "#ffddba",
+        "entry_bg": "#6B6666",
+        "entry_fg": "#ffddba",
+        "btn_bg": "#4f4c4c"
+    },
+    "Sunrise": {
+        "bg": "#F5E9D3",
+        "fg": "#6B4226",
+        "entry_bg": "#FAF5EA",
+        "entry_fg": "#6B4226",
+        "btn_bg": "#ECDFCA"
+    },
+    "Sunset": {
+        "bg": "#2F2C3D",
+        "fg": "#B5BCFF",
+        "entry_bg": "#5D5879",
+        "entry_fg": "#B5BCFF",
+        "btn_bg": "#49455F"
+    },
+    "Midnight": {
+        "bg": "#09141F",
+        "fg": "#ECD1C5",
+        "entry_bg": "#0F2133",
+        "entry_fg": "#ECD1C5",
+        "btn_bg": "#0C1A29"
+    },
+    "Wine": {
+        "bg": "#251920",
+        "fg": "#f7ebed",
+        "entry_bg": "#532333",
+        "entry_fg": "#f7ebed",
+        "btn_bg": "#3e1a26"
+    },
+}
